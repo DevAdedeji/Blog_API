e3b16b1e27540dfc6b9e875a74111eab006a9f92
DevAdedeji <adedejitewogbade2@gmail.com>
2022-05-30 23:05:17 +0100

    Done

--- models/model.go
+++ models/model.go
@@ -3,14 +3,8 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Blog struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title    string             `json:"title"`
-	Body     string             `json:"body"`
-	Author   string             `json:"author"`
-	Comments []Comment          `json:"comments,omitempty"`
-}
-
-type Comment struct {
-	Name    string `json:"name"`
-	Comment string `json:"comment"`
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title  string             `json:"title"`
+	Body   string             `json:"body"`
+	Author string             `json:"author"`
 }
